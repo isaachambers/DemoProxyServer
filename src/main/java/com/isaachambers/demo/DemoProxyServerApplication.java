89acd142a18519e5f0a45db60da5eb1f4033a0cd
isaachambers <isaaceduardk@gmail.com>
2019-05-06 11:55:52 +0300

    Basic Zuul Project Setup

---
+++ src/main/java/com/isaachambers/demo/DemoProxyServerApplication.java
@@ -0,0 +1,13 @@
+package com.isaachambers.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProxyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProxyServerApplication.class, args);
+	}
+
+}

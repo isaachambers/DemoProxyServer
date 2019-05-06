266b10451aaa3c753c8c609cb9af66f0e629abb2
isaachambers <isaaceduardk@gmail.com>
2019-05-06 12:02:47 +0300

    Basic Zuul Project Setup

--- src/main/java/com/isaachambers/demo/DemoProxyServerApplication.java
+++ src/main/java/com/isaachambers/demo/DemoProxyServerApplication.java
@@ -2,8 +2,12 @@ package com.isaachambers.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class DemoProxyServerApplication {
 
 	public static void main(String[] args) {
